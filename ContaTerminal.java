3eb88474aa43c053f5e637933bc57553bbe7e098
Renan Oliveira <111541362+Souzas2001@users.noreply.github.com>
2024-05-07 10:03:29 -0300

    Update ContaTerminal.java

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -7,15 +7,15 @@ public class ContaTerminal {
         int numero = entrada.nextInt();
 
         System.out.println("Por favor, digite o número da agencia! ");
-        String agencia = entrada.nextLine();
+        String agencia = entrada.next();
 
-        System.err.println("Por favor, digite o seu nome! ");
-        String nome = entrada.nextLine();
+        System.out.println("Por favor, digite o seu nome! ");
+        String nome = entrada.next();
 
         System.out.println("Por favor, digite o saldo ");
         Float saldo = entrada.nextFloat();    
         
-        System.out.println("Olá" + nome + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        System.out.println("Olá" + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
         " , conta " + numero + " e seu saldo " + saldo + " já está disponível para saque! " );
     }
 }
